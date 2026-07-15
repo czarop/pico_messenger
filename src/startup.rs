74a110f418379d55b8c6a6f5fb933785a6b8d2ff
czarop <151532746+czarop@users.noreply.github.com>
2026-07-15 10:39:56 +0100

    1

--- src/startup.rs
+++ src/startup.rs
@@ -92,157 +92,157 @@ pub async fn startup(spawner: Spawner) {
     );
     let i2c0_bus = I2C0_BUS.init(Mutex::new(i2c0));
 
-    // let i2c1 = I2c::new_async(
-    //     p.I2C1,
-    //     p.PIN_7,
-    //     p.PIN_6,
-    //     Irqs,
-    //     embassy_rp::i2c::Config::default(),
-    // );
-    // let i2c1_bus = I2C1_BUS.init(Mutex::new(i2c1));
+    let i2c1 = I2c::new_async(
+        p.I2C1,
+        p.PIN_7,
+        p.PIN_6,
+        Irqs,
+        embassy_rp::i2c::Config::default(),
+    );
+    let i2c1_bus = I2C1_BUS.init(Mutex::new(i2c1));
 
-    // let i2c_for_bno085: i2c::I2cDevice<
-    //     '_,
-    //     CriticalSectionRawMutex,
-    //     I2c<'static, I2C1, embassy_rp::i2c::Async>,
-    // > = i2c::I2cDevice::new(i2c1_bus);
+    let i2c_for_bno085: i2c::I2cDevice<
+        '_,
+        CriticalSectionRawMutex,
+        I2c<'static, I2C1, embassy_rp::i2c::Async>,
+    > = i2c::I2cDevice::new(i2c1_bus);
 
-    // let i2c_for_display: i2c::I2cDevice<
-    //     '_,
-    //     CriticalSectionRawMutex,
-    //     I2c<'static, I2C0, embassy_rp::i2c::Async>,
-    // > = i2c::I2cDevice::new(i2c0_bus);
-    // let i2c_for_battery_monitor: i2c::I2cDevice<
-    //     '_,
-    //     CriticalSectionRawMutex,
-    //     I2c<'static, I2C0, embassy_rp::i2c::Async>,
-    // > = i2c::I2cDevice::new(i2c0_bus);
-    // let i2c_for_temp_sensor: i2c::I2cDevice<
-    //     '_,
-    //     CriticalSectionRawMutex,
-    //     I2c<'static, I2C0, embassy_rp::i2c::Async>,
-    // > = i2c::I2cDevice::new(i2c0_bus);
-    // let mut i2c_for_pressure_sensor: i2c::I2cDevice<
-    //     '_,
-    //     CriticalSectionRawMutex,
-    //     I2c<'static, I2C0, embassy_rp::i2c::Async>,
-    // > = i2c::I2cDevice::new(i2c0_bus);
+    let i2c_for_display: i2c::I2cDevice<
+        '_,
+        CriticalSectionRawMutex,
+        I2c<'static, I2C0, embassy_rp::i2c::Async>,
+    > = i2c::I2cDevice::new(i2c0_bus);
+    let i2c_for_battery_monitor: i2c::I2cDevice<
+        '_,
+        CriticalSectionRawMutex,
+        I2c<'static, I2C0, embassy_rp::i2c::Async>,
+    > = i2c::I2cDevice::new(i2c0_bus);
+    let i2c_for_temp_sensor: i2c::I2cDevice<
+        '_,
+        CriticalSectionRawMutex,
+        I2c<'static, I2C0, embassy_rp::i2c::Async>,
+    > = i2c::I2cDevice::new(i2c0_bus);
+    let mut i2c_for_pressure_sensor: i2c::I2cDevice<
+        '_,
+        CriticalSectionRawMutex,
+        I2c<'static, I2C0, embassy_rp::i2c::Async>,
+    > = i2c::I2cDevice::new(i2c0_bus);
 
-    // i2c_scan(&mut i2c_for_pressure_sensor).await;
+    i2c_scan(&mut i2c_for_pressure_sensor).await;
 
-    // let mut display = screen::Display::new(i2c_for_display).await;
-    // let mut temp_senor = temp_sensor::TempSensor::new(i2c_for_temp_sensor);
-    // let mut max17048 = battery_meter::Max17048::new(i2c_for_battery_monitor);
-    // let my_altitude_known = 21.0;
-    // let mut pressure_sensor =
-    //     crate::sensors::altimeter::Altimeter::new(i2c_for_pressure_sensor, Some(my_altitude_known))
-    //         .await
-    //         .expect("could not initiate pressure sensor");
-    // let mut bno085 = bno085::Imu::new(i2c_for_bno085, p.PIN_3, p.PIN_2).await;
+    let mut display = screen::Display::new(i2c_for_display).await;
+    let mut temp_senor = temp_sensor::TempSensor::new(i2c_for_temp_sensor);
+    let mut max17048 = battery_meter::Max17048::new(i2c_for_battery_monitor);
+    let my_altitude_known = 21.0;
+    let mut pressure_sensor =
+        crate::sensors::altimeter::Altimeter::new(i2c_for_pressure_sensor, Some(my_altitude_known))
+            .await
+            .expect("could not initiate pressure sensor");
+    let mut bno085 = bno085::Imu::new(i2c_for_bno085, p.PIN_3, p.PIN_2).await;
 
-    // bno085
-    //     .enable_rotation_vector(1000)
-    //     .await
-    //     .expect("Failed to enable rotation vector");
-    // // bno085.enable_activity_recognition().await.expect("failed to initiate activity type");
-    // bno085
-    //     .enable_significant_motion_wake()
-    //     .await
-    //     .expect("failed to enable shake detection");
+    bno085
+        .enable_rotation_vector(1000)
+        .await
+        .expect("Failed to enable rotation vector");
+    // bno085.enable_activity_recognition().await.expect("failed to initiate activity type");
+    bno085
+        .enable_significant_motion_wake()
+        .await
+        .expect("failed to enable shake detection");
 
-    // spawner
-    //     .spawn(bno085::imu_task(bno085, IMU_REPORTS.sender()).expect("failed to spawn imu task"));
+    spawner
+        .spawn(bno085::imu_task(bno085, IMU_REPORTS.sender()).expect("failed to spawn imu task"));
 
-    // spawner.spawn(bno085::ui_task(IMU_REPORTS.receiver()).expect("failed to spawn imu task"));
+    spawner.spawn(bno085::ui_task(IMU_REPORTS.receiver()).expect("failed to spawn imu task"));
 
-    // info!("entering loop");
+    info!("entering loop");
 
-    // loop {
-    //     // ENTER_SLEEP.signal(());
+    loop {
+        // ENTER_SLEEP.signal(());
 
-    //     let soc = match max17048.soc().await {
-    //         Ok(soc) => soc,
-    //         Err(e) => {
-    //             // error!("Failed to read state of charge: {:?}", e);
-    //             0
-    //         }
-    //     };
+        let soc = match max17048.soc().await {
+            Ok(soc) => soc,
+            Err(e) => {
+                // error!("Failed to read state of charge: {:?}", e);
+                0
+            }
+        };
 
-    //     let is_charging = match max17048.charge_rate().await {
-    //         Ok(rate) => {
-    //             info!("Charge rate: {}%/hr", rate);
-    //             rate > 0.0
-    //         }
-    //         Err(e) => {
-    //             // error!("Failed to read charge rate: {:?}", e);
-    //             false
-    //         }
-    //     };
+        let is_charging = match max17048.charge_rate().await {
+            Ok(rate) => {
+                info!("Charge rate: {}%/hr", rate);
+                rate > 0.0
+            }
+            Err(e) => {
+                // error!("Failed to read charge rate: {:?}", e);
+                false
+            }
+        };
 
-    //     let (temp_reading, humidity_reading) = match temp_senor
-    //         .read_temperature(temp_sensor::TempSensorPowerMode::LPM3)
-    //         .await
-    //     {
-    //         Ok(r) => {
-    //             let mut temp: String<24> = String::new();
-    //             core::write!(temp, "Temp: {:.1}C", r.temperature).unwrap();
-    //             let mut humidity: String<24> = String::new();
-    //             core::write!(humidity, "Humidity: {:.1}%", r.humidity).unwrap();
-    //             (Some(temp), Some(humidity))
-    //         }
-    //         Err(e) => {
-    //             error!("{:?}", defmt::Debug2Format(&e));
-    //             let err_string: String<24> =
-    //                 String::from_str("Temp Senor Error").expect("error making error string");
-    //             (Some(err_string), None)
-    //         }
-    //     };
-    //     let battery_level = battery_meter::BatteryLevel::from_soc(soc, is_charging);
+        let (temp_reading, humidity_reading) = match temp_senor
+            .read_temperature(temp_sensor::TempSensorPowerMode::LPM3)
+            .await
+        {
+            Ok(r) => {
+                let mut temp: String<24> = String::new();
+                core::write!(temp, "Temp: {:.1}C", r.temperature).unwrap();
+                let mut humidity: String<24> = String::new();
+                core::write!(humidity, "Humidity: {:.1}%", r.humidity).unwrap();
+                (Some(temp), Some(humidity))
+            }
+            Err(e) => {
+                error!("{:?}", defmt::Debug2Format(&e));
+                let err_string: String<24> =
+                    String::from_str("Temp Senor Error").expect("error making error string");
+                (Some(err_string), None)
+            }
+        };
+        let battery_level = battery_meter::BatteryLevel::from_soc(soc, is_charging);
 
-    //     let altitude = match pressure_sensor.read_altitude().await {
-    //         Ok(alt) => {
-    //             let mut alt_str: String<24> = String::new();
-    //             core::write!(alt_str, "Alt: {:.1}m", alt).unwrap();
-    //             Some(alt_str)
-    //         }
-    //         Err(e) => {
-    //             error!("Failed to read altitude: {:?}", e);
-    //             None
-    //         }
-    //     };
+        let altitude = match pressure_sensor.read_altitude().await {
+            Ok(alt) => {
+                let mut alt_str: String<24> = String::new();
+                core::write!(alt_str, "Alt: {:.1}m", alt).unwrap();
+                Some(alt_str)
+            }
+            Err(e) => {
+                error!("Failed to read altitude: {:?}", e);
+                None
+            }
+        };
 
-    //     let display_info = StatusScreen {
-    //         battery: battery_level,
-    //         message: [
-    //             temp_reading.clone(),
-    //             humidity_reading.clone(),
-    //             Some(
-    //                 heapless::String::<24>::from_str("Updated!")
-    //                     .expect("could not make heapless string"),
-    //             ),
-    //             altitude,
-    //             None,
-    //         ],
-    //     };
-    //     let _ = display.show_message(display_info).await;
+        let display_info = StatusScreen {
+            battery: battery_level,
+            message: [
+                temp_reading.clone(),
+                humidity_reading.clone(),
+                Some(
+                    heapless::String::<24>::from_str("Updated!")
+                        .expect("could not make heapless string"),
+                ),
+                altitude,
+                None,
+            ],
+        };
+        let _ = display.show_message(display_info).await;
 
-    //     embassy_time::Timer::after(embassy_time::Duration::from_secs(15)).await;
+        embassy_time::Timer::after(embassy_time::Duration::from_secs(15)).await;
 
-    //     // let display_info = StatusScreen {
-    //     //     battery: battery_level,
-    //     //     message: [
-    //     //         temp_reading,
-    //     //         humidity_reading,
-    //     //         Some(
-    //     //             heapless::String::<24>::from_str("Shake to update")
-    //     //                 .expect("could not make heapless string"),
-    //     //         ),
-    //     //         None,
-    //     //         None,
-    //     //     ],
-    //     // };
-    //     // let _ = display.show_message(display_info).await;
-    // }
+        // let display_info = StatusScreen {
+        //     battery: battery_level,
+        //     message: [
+        //         temp_reading,
+        //         humidity_reading,
+        //         Some(
+        //             heapless::String::<24>::from_str("Shake to update")
+        //                 .expect("could not make heapless string"),
+        //         ),
+        //         None,
+        //         None,
+        //     ],
+        // };
+        // let _ = display.show_message(display_info).await;
+    }
 
     let i2c_for_rtc = i2c::I2cDevice::new(i2c0_bus);
     match crate::rtc::Pcf8523::new(i2c_for_rtc).await {
